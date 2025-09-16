142a87b1347b54977080b460e9dde3ad8bd1c55d
mmejiatech10 <mmejia.tech10@gmail.com>
2025-09-15 20:01:42 -0500

    cambio

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner animal = new Scanner(System.in);
+        ArrayList<perro> listPerro = new ArrayList<>();
+        perro.add (new perro ("terri","pincher",false));
+
+
+
+
+        int opc;
+        do {
+            System.out.println("MENU PRINCIPAL ");
+            System.out.println("1.QUIERE AGREGAR PERRO");
+            System.out.println("2.QUIERE BUSCAR PERRO POR ID ");
+            System.out.println("3.QUIERE REGISTRA ADOPCION");
+            System.out.println("4.QUIERE VACUNAR AL PERRO ");
+            System.out.println("5.QUIERE ESTERILIZAR EL PERRO ");
+            System.out.println("6.QUIERE LISTAR PERROS DISPONIBLES ");
+            System.out.println("7.QUIERE ELIMINAR PERRO POR ID ");
+            System.out.println("8.QUIERE LISTAR TODOS LOS PERROS ");
+            System.out.println("9.QUIERE SALIR O TERMINAR ");
+
+            switch (opc){
+                case 1:
+                perro est1 = new perro();
+                    System.out.println("ingrese el nombre del perro ");
+                    String nombre = animal.nextLine();
+
+                    System.out.println("ingrese la raza del perro");
+                    String raza = animal.nextLine();
+
+                    System.out.println("ingrese la edad en meses ");
+                    int edadmeses =  animal.nextInt();
+
+                    System.out.println("ingrese el tamaño (Grande,Mediano,Pequeño");
+                    String tamaño = animal.nextLine();
+
+                    System.out.println("esta vacunado el perro ");
+                    Boolean vacunado = animal.nextBoolean();
+
+                    System.out.println("esta esterilizado el perro ");
+                    boolean esterilizado = animal.nextBoolean();
+
+
+                    break;
+
+                case 2 :
+                    System.out.println("Digite porfavor el ID del perro ");
+                    int id = animal.nextInt();
+                    return toString();
+
+
+                    break;
+                case 3 :
+                    System.out.println("cual es el id del perro ");
+                    int id = animal.nextInt();
+                    System.out.println("cual es el nombre del adoptante ");
+                    String nombre = animal.nextLine();
+
+
+                    break;
+
+                case 4 :
+                    System.out.println(" cual es el ID del perro ");
+                    int id = animal.nextInt();
+
+                    perro.vacunar();
+
+
+                    break;
+
+                case 5 :
+
+                    System.out.println(" cual es el ID del perro ");
+                    int id = animal.nextInt();
+
+                    perro.esterilizar();
+
+                    break;
+                case 6 :
+
+                    break;
+
+                case 7:
+                    System.out.println("cual es el ID que quiere eliminar ");
+
+                    break;
+
+                case 8:
+
+                    break;
+
+                case 9:
+                    System.out.println("saliendo");
+                    break;
+                default:
+                    System.out.println("numero invalido");
+            }
+
+        } while (opc != 9);
+
+
+    }
+}
---
+++ src/perro.java
@@ -0,0 +1,135 @@
+public class perro {
+
+
+    private int id;
+    private String nombre;
+    private String raza;
+    private int edadmeses;
+    private String tamaño;
+    private boolean vacunado;
+    private boolean esterilizado;
+    private boolean adoptado;
+    private  String adoptante;
+
+    public perro(int id, String nombre, String raza, int edadmeses, String tamaño, boolean vacunado, boolean esterilizado, boolean adoptado, String adoptante) {
+        this.id = id;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edadmeses = edadmeses;
+        this.tamaño = tamaño;
+        this.vacunado = vacunado;
+        this.esterilizado = esterilizado;
+        this.adoptado = adoptado;
+        this.adoptante = adoptante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdadmeses() {
+        return edadmeses;
+    }
+
+    public void setEdadmeses(int edadmeses) {
+        this.edadmeses = edadmeses;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public boolean isVacunado() {
+        return vacunado;
+    }
+
+    public void setVacunado(boolean vacunado) {
+        this.vacunado = vacunado;
+    }
+
+    public boolean isEsterilizado() {
+        return esterilizado;
+    }
+
+    public void setEsterilizado(boolean esterilizado) {
+        this.esterilizado = esterilizado;
+    }
+
+    public boolean isAdoptado() {
+        return adoptado;
+    }
+
+    public void setAdoptado(boolean adoptado) {
+        this.adoptado = adoptado;
+    }
+
+    public String getAdoptante() {
+        return adoptante;
+    }
+
+    public void setAdoptante(String adoptante) {
+        this.adoptante = adoptante;
+    }
+
+    public vacunar(){
+
+    }
+
+    public esterilizar(){
+
+    }
+
+    public marcaradoptados(String adopatante){
+
+    }
+
+    public calcularedadhumanaaprox(){
+
+
+    }
+
+    public aptoparaapartamento(){
+
+
+    }
+    public String toString(){
+        return "Perro" +
+                "id=" + id +
+                ", nombre=" + nombre +
+                ", raza=" + raza +
+                ", edadMeses=" + edadmeses +
+                ", tamano='" + tamaño + '\'' +
+                ", vacunado=" + vacunado +
+                ", esterilizado=" + esterilizado +
+                ", adoptado=" + adoptado +
+                ", adoptante='" + adoptante + '
+
+
+
+    }
+
+}
